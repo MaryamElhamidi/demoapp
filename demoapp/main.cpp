e829514a701157e466b8fb85c70c5e95938f2045
MaryamElhamidi <unicolmaryame@gmail.com>
2025-04-13 02:34:56 -0400

    Initial Commit

---
+++ demoapp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  demoapp
+//
+//  Created by Maryam Elhamidi on 2025-04-13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
